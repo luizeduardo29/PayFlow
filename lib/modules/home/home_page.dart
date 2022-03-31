90c23733ee4dfc72bc591fef06f154bc40e91261
Luiz Eduardo <davila.dudu@hotmail.com>
2022-03-31 18:39:50 -0300

    Second commit

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -78,8 +78,8 @@ class _HomePageState extends State<HomePage> {
                 )),
             GestureDetector(
               onTap: () async {
-                await Navigator.pushNamed(context, "/barcode_scanner");
-                //await Navigator.pushNamed(context, "/insert_boleto");
+                //await Navigator.pushNamed(context, "/barcode_scanner");
+                await Navigator.pushNamed(context, "/insert_boleto");
                 setState(() {});
               },
               child: Container(
